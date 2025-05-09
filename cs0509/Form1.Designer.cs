9a7ff2fda5a217521f8d0e0eed3590f250789a72
YuTanaka <tanakaedu@users.noreply.github.com>
2025-05-09 16:07:54 +0900

    移動

--- cs0509/Form1.Designer.cs
+++ cs0509/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             buttonProperty = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
             SuspendLayout();
             // 
             // buttonProperty
@@ -36,28 +42,102 @@
             buttonProperty.BackColor = Color.FromArgb(192, 255, 192);
             buttonProperty.Font = new Font("メイリオ", 32F);
             buttonProperty.ForeColor = Color.FromArgb(0, 64, 0);
-            buttonProperty.Location = new Point(214, 108);
+            buttonProperty.Location = new Point(12, 12);
             buttonProperty.Name = "buttonProperty";
-            buttonProperty.Size = new Size(400, 100);
+            buttonProperty.Size = new Size(402, 67);
             buttonProperty.TabIndex = 0;
             buttonProperty.Text = "ボタン";
             buttonProperty.UseVisualStyleBackColor = false;
             buttonProperty.Click += button1_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(12, 85);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 62);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F);
+            label1.Location = new Point(367, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 45);
+            label1.TabIndex = 2;
+            label1.Text = "(・ω・)";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(752, 346);
+            button2.Name = "button2";
+            button2.Size = new Size(54, 53);
+            button2.TabIndex = 3;
+            button2.Text = "UP";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(704, 405);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 51);
+            button3.TabIndex = 4;
+            button3.Text = "LEFT";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(802, 405);
+            button4.Name = "button4";
+            button4.Size = new Size(51, 51);
+            button4.TabIndex = 5;
+            button4.Text = "RIGHT";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(752, 462);
+            button5.Name = "button5";
+            button5.Size = new Size(54, 42);
+            button5.TabIndex = 6;
+            button5.Text = "DOWN";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(865, 520);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Controls.Add(buttonProperty);
-            ForeColor = Color.Red;
+            ForeColor = SystemColors.ActiveCaptionText;
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button buttonProperty;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
--- cs0509/Form1.cs
+++ cs0509/Form1.cs
@@ -15,6 +15,38 @@ namespace cs0509
             buttonProperty.Text = "‰Ÿ‚³‚ê‚½";
             buttonProperty.Left = 0;
             buttonProperty.Visible = true;
+            label1.Visible = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Left = button1.Left + 10;
+            button1.Top = button1.Top + 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
         }
     }
 }
