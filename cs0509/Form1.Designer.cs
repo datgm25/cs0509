7c7761e03eadeb6c95a9ea097ae532cd0fe2685c
YuTanaka <tanakaedu@users.noreply.github.com>
2025-05-09 14:29:09 +0900

    プロパティ

--- cs0509/Form1.Designer.cs
+++ cs0509/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 192);
+            button1.Font = new Font("メイリオ", 32F);
+            button1.ForeColor = Color.FromArgb(0, 64, 0);
+            button1.Location = new Point(56, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 100);
+            button1.TabIndex = 0;
+            button1.Text = "ボタン";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            ForeColor = Color.Red;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
